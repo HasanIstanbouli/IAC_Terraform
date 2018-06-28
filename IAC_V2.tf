b5c0d7a744ada7dbb61fd18098ac6685003d0f27
Hassan Istanbouli <hassanistanbouli@gmail.com>
2018-06-29 01:15:07 +0300

    add my project

---
+++ IAC_V2.tf
@@ -0,0 +1,109 @@
+provider "openstack" {
+  user_name   = "admin"
+  tenant_name = "admin"
+  password    = "123123"
+  auth_url    = "http://10.0.0.11:5000/v3"
+  region      = "RegionOne"
+  domain_name = "default"
+}
+
+#Here we create a router with external_gateway -------------
+
+resource "openstack_networking_router_v2" "router_terraform" {
+ name                = "router_terraform"
+  admin_state_up      = true
+  external_network_id = "bfd56918-cfab-4d06-a5b3-c890f2ac648b"
+}
+
+
+#Here we create an internal network --------------------
+
+resource "openstack_networking_network_v2" "internal_terraform" {
+  name           = "internal_terraform"
+  admin_state_up = "true"
+}
+
+#Here we associate this subnet to the "internal_terraform"-----------------
+
+resource "openstack_networking_subnet_v2" "subnet_1_Terraform" {
+  name       = "subnet_1_Terraform"
+  network_id = "${openstack_networking_network_v2.internal_terraform.id}"
+  cidr       = "192.168.1.0/24"
+  ip_version = 4
+  enable_dhcp = "true"
+  depends_on =["openstack_networking_network_v2.internal_terraform"]
+}
+
+#Here we create an interface associated between router and network----------
+
+resource "openstack_networking_router_interface_v2" "router_interface_1" {
+  router_id = "${openstack_networking_router_v2.router_terraform.id}"
+  subnet_id = "${openstack_networking_subnet_v2.subnet_1_Terraform.id}"
+  depends_on= ["openstack_networking_subnet_v2.subnet_1_Terraform"]
+  depends_on =["openstack_networking_router_v2.router_terraform"]
+}
+
+resource "openstack_compute_instance_v2"  "Web_Server_" {
+	count = 2
+	name = "Web_Server_${count.index}"
+	image_id = "b4d7009f-5071-46c8-b16d-5f38e2589aef"
+	flavor_id = "0"
+	security_groups = ["default"]
+	
+		  network {
+	  
+		name = "internal_terraform"
+	}
+	
+	depends_on=["openstack_networking_subnet_v2.subnet_1_Terraform"]
+}
+resource "openstack_compute_instance_v2"  "Load_Balancer_Server" {
+	name = "Load_Balancer_Server"
+	image_id = "b4d7009f-5071-46c8-b16d-5f38e2589aef"
+	flavor_id = "0"
+	security_groups = ["default"]
+	
+		  network {
+	  
+		name = "internal_terraform"
+	}
+	depends_on=["openstack_networking_subnet_v2.subnet_1_Terraform"]
+}
+#Here we create a second internal network --------------------
+
+resource "openstack_networking_network_v2" "internal_terraform_2" {
+  name           = "internal_terraform_2"
+  admin_state_up = "true"
+}
+#Here we associate this subnet to the "internal_terraform_2"-------------------------
+
+resource "openstack_networking_subnet_v2" "subnet_2_Terraform" {
+  name       = "subnet_2_Terraform"
+  network_id = "${openstack_networking_network_v2.internal_terraform_2.id}"
+  cidr       = "10.0.0.0/24"
+  ip_version = 4
+  enable_dhcp = "true"
+  depends_on=["openstack_networking_network_v2.internal_terraform_2"]
+}
+
+#Here we create an interface associated between router and network-------------------------
+resource "openstack_networking_router_interface_v2" "router_interface_2" {
+  router_id = "${openstack_networking_router_v2.router_terraform.id}"
+  subnet_id = "${openstack_networking_subnet_v2.subnet_2_Terraform.id}"
+  depends_on= ["openstack_networking_subnet_v2.subnet_2_Terraform"]
+  depends_on =["openstack_networking_router_v2.router_terraform"]
+  
+}
+resource "openstack_compute_instance_v2"  "DB_Server_" {
+	count = 2
+	name = "DB_Server_${count.index}"
+	image_id = "b4d7009f-5071-46c8-b16d-5f38e2589aef"
+	flavor_id = "0"
+	security_groups = ["default"]
+	
+		  network {
+	  
+		name = "internal_terraform_2"
+	}
+	depends_on =["openstack_networking_subnet_v2.subnet_2_Terraform"]
+}
